ebd43ae1ce5ffae3e7672da371d389a0269da196
Arunachalam <36620373+arunrio@users.noreply.github.com>
2018-05-16 11:04:34 +0530

    Update posneg.py

--- BeginnerLevel/posneg.py
+++ BeginnerLevel/posneg.py
@@ -1,7 +1,7 @@
 num = float(input("Enter a number: "))
 if (1<= num <= 100000) :
-   print("Positive number")
+   print("Positive")
 elif num == 0:
    print("Zero")
 else:
-   print("Negative number")
+   print("Negative")
