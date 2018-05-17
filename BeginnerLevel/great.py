2e1cb1adc9e179298a8401cdfb891a8dbb659361
Arunachalam <36620373+arunrio@users.noreply.github.com>
2018-05-17 09:51:34 +0530

    Update great.py

--- BeginnerLevel/great.py
+++ BeginnerLevel/great.py
@@ -1,10 +1,10 @@
-a=int(input("Enter the number1:"))
-b=int(input("Enter the number2:"))
-c=int(input("Enter the number3:"))
-if(a>b):
-  if(a>c):
-    print(a)
-elif(b>c):
-  print(b)
+a1=int(input("Enter the number1:"))
+a2=int(input("Enter the number2:"))
+a3=int(input("Enter the number3:"))
+if(a1>a2):
+  if(a1>a3):
+    print(a1)
+elif(a2>a3):
+  print(a2)
 else:
-  print(c)
+  print(a3)
